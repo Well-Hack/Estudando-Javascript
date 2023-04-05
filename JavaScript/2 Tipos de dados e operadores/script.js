803172a74715d3ba25289b3808bb14525d69916c
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-04-05 16:23:05 -0300

    operações aritmeticas
    operações aritmeticas

--- JavaScript/2 Tipos de dados e operadores/script.js
+++ JavaScript/2 Tipos de dados e operadores/script.js
@@ -1,3 +1,6 @@
-console.log(typeof 5);
-console.log(typeof 12.7);
-console.log(typeof '-123');
+console.log(2 + 3);
+console.log(5 - 12);
+console.log(4 * 4);
+console.log(5 / 3);
+console.log(20 % 2);
+console.log(18 % 35);
