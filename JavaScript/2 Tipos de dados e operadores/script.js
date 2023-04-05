d0574e3849e319fdf87846ef93c7e434ff78ce93
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-04-05 16:46:48 -0300

    2.6 tipos d edados: booleans

--- JavaScript/2 Tipos de dados e operadores/script.js
+++ JavaScript/2 Tipos de dados e operadores/script.js
@@ -1,5 +1,3 @@
-console.log("Oi, \n meu nome é Wellerson");
-console.log('Ola, isso é um "teste" para botar aspas')
-console.log("salada " + "de " + "fruta")
-console.log(`A soma de 2 + 2 é: ${2+2}`)
+console.log(5 > 3)
+console.log(typeof false)
 
