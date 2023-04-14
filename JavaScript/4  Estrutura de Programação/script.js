e18c99556b0d1cc4e0ecf9639d8fe8b4cebb8c53
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-04-14 19:04:48 -0300

    Estrutura de repetição while

--- JavaScript/4  Estrutura de Programação/script.js
+++ JavaScript/4  Estrutura de Programação/script.js
@@ -1,10 +1,7 @@
 
-let idade = window.prompt('Qual a sua idade? ')
+let idade = 10
 
-if (idade < 18){
-    window.alert("Você é de menor");
-}else if(idade >= 18 && idade <= 21){
-    window.alert("Você esta na melhor idade");
-}else{
-    window.alert("Vocẽ está ficando velho")
+while(idade < 19){
+    console.log(`A idade está em ${idade}`);
+    idade += 1;
 }
