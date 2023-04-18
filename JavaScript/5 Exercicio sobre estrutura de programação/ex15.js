365ae8a08f9177d429566accaa415e83e6f0af48
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-04-18 19:12:06 -0300

    ex15

--- JavaScript/5 Exercicio sobre estrutura de programação/ex15.js
+++ JavaScript/5 Exercicio sobre estrutura de programação/ex15.js
@@ -5,3 +5,19 @@ si proprio e por 1;
 */
 
 
+let num = 15;
+let div = 0;
+
+for(let i = 1; i <= num; i++){
+
+    if(num % i == 0){
+        div++;
+    }
+}
+
+
+if(div == 2){
+    console.log(`O número ${num} é primo`)
+}else{
+    console.log(`O numero ${num} não é primo`)
+}
