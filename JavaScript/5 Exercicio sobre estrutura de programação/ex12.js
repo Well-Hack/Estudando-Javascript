4ab80f2d17742b22c0af8b70a5f0c7f04f57066a
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-04-18 18:41:00 -0300

    exercicio11

--- JavaScript/5 Exercicio sobre estrutura de programação/ex12.js
+++ JavaScript/5 Exercicio sobre estrutura de programação/ex12.js
@@ -1,15 +1,6 @@
-/*Exercicio 10
+/*Exercicio 12
 *Armazene a velocidade de um carro em uma variável, com o número que deseja;
 *Faça uma estrutura if/else que verifica se ele está acima da velocidade;
 *80 é a velocidade máxima permitida;
 *Se estiver acima ou abaixo exiba mensagens com o console.log
 */
-
-let vel = 80
-
-if(vel <= 120){
-    console.log("Dentro da velocidade permitida")
-}else{
-    console.log(`Você está a ${vel}Km/h, está á ${vel - 120}Km/h acima do limite`)
-}
-
