cb1e16303324ea6981bf67615996fc29c289efbe
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-04-18 18:25:42 -0300

    Exercicio 08

---
+++ JavaScript/5 Exercicio sobre estrutura de programação/ex08.js
@@ -0,0 +1,14 @@
+/*Exercicio 08
+*Crie uma constante com o seu nome como valor;
+*Depois uma estrutura if que verifica se o seu nome é o que está
+na constante;
+*Se estiver, emita uma mensagem de saudação com console.log();
+*/
+
+const nome = "Wellerson"
+
+if(nome == "Wellerson"){
+    console.log(`Olá ${nome} seja bem vindo`)
+}else{
+    console.log("Pessoa não identificada")
+}
