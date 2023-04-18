72a2942916b71c381bd7a1eacfea51b6319e0c79
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-04-18 18:46:22 -0300

    exercicio 11

---
+++ JavaScript/5 Exercicio sobre estrutura de programação/ex11.js
@@ -0,0 +1,19 @@
+/*Exercicio 11
+*Faça uma estrutura if/else para verificar se um usuário pode dirigir;
+*Armazene em variáveis algumas informações sobre o usuário:
+idade - se tem CNH
+*Se a idade for maior que 18 e não possuir CNH, exiba uma mensagem;
+Se a idade for maior que 18 e tem CNH, exiba uma mensagem;
+*Se não tiver 18 nem CNH, exiba outra mensage;
+*/
+
+let idade = 15;
+let cnh = false;
+
+if(idade >= 18 && cnh == false){
+    console.log("Você já é de maior, já pode tirar a CNH")
+} else if(idade >= 18 && cnh == true){
+    console.log("Você está apto para dirigir")
+}else{
+    console.log("Você ainda não tem idade para dirigir")
+}
