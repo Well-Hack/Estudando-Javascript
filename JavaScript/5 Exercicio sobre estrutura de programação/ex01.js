5ce12e7872cd37e2f6ac99ec0e6da1232e04609e
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-04-18 18:08:58 -0300

    exercicio sobre estrutura de programação
    exercicio 06

---
+++ JavaScript/5 Exercicio sobre estrutura de programação/ex01.js
@@ -0,0 +1,12 @@
+/*Exercicio sobre estrutura de programação 1
+    Armazene em valores em variáveis com cada um dos tipos de dados vistos,
+    String, Number e Boolean
+*/
+
+let s = "Palavra";
+let n = 5;
+let b = Null;
+
+console.log(typeof(s))
+console.log(typeof(n))
+console.log(typeof(b))
