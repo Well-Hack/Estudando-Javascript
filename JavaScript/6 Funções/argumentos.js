1b5597ce30b9e85e9c62fdc2d9b1117ad0215797
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-04-21 16:58:34 -0300

    argumentos opcionais

--- JavaScript/6 Funções/argumentos.js
+++ JavaScript/6 Funções/argumentos.js
@@ -9,3 +9,18 @@ function soma(a,b){
 
 console.log(soma(1));
 console.log(soma(1,2));
+
+
+//---------------------------
+
+
+function saudacao(nome,idade){
+    if(idade == undefined){
+        console.log("Olá " + nome);
+    }else{
+        console.log(`Olá ${nome} você tem ${idade} anos`)
+    }
+} 
+
+saudacao("Mateus")
+saudacao("Igor",29)
