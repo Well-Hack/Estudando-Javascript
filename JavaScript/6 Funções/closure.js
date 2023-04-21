374754e6a4fb618fe63606a3cddc686c55c8fabc
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-04-21 17:14:38 -0300

    closure
    foi o mais dificil

--- JavaScript/6 Funções/closure.js
+++ JavaScript/6 Funções/closure.js
@@ -11,3 +11,23 @@ console.log(soma1(5));
 let soma2 = lembrar_soma(5);
 
 console.log(soma2(7));
+
+//---------------------------------
+
+function contador(i){
+    let cont = i;
+    let somar_contador = function(){
+        console.log(cont);
+        cont++;
+    }
+    return somar_contador;
+}
+
+let meu_contador = contador(5);
+
+meu_contador();
+meu_contador();
+meu_contador();
+meu_contador();
+meu_contador();
+meu_contador();
