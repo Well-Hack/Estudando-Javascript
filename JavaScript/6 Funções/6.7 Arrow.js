153a11a8bd5585872b820d3b4e07d0782d506a9f
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-04-21 16:47:32 -0300

    arrow function

---
+++ JavaScript/6 Funções/6.7 Arrow.js
@@ -0,0 +1,18 @@
+let console_teste = () => {
+    console.log("Olá Mundo")
+}
+
+console_teste()
+
+// ----------------------------------
+
+let soma = (a,b) => {
+    return a + b;
+};
+
+
+console.log(soma(2,7));
+
+
+//-----------------------------------
+
