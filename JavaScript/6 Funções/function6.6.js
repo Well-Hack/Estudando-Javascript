4e6ef39efe8ad06a351b2ab344c303a56a2385f7
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-04-21 16:39:34 -0300

    6.6 escopos aninhados

---
+++ JavaScript/6 Funções/function6.6.js
@@ -0,0 +1,16 @@
+let x = 7;
+
+function multiplique(a,b) {
+    let x = a * b;
+
+    if(x > 10){
+        let x = 0;
+
+        console.log(x);
+    }
+    console.log(x);
+}
+
+console.log(x)
+
+multiplique(3,5);
