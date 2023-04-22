011867aac04cf8bbe175700ee8f5723e7f6c22c5
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-04-22 14:22:02 -0300

    exercicio 17

---
+++ JavaScript/7 Exercicio sobre funções/exerc17.js
@@ -0,0 +1,12 @@
+/*Exercicio 17
+*Escreva uma função que recebe um parâmetro de idade;
+*E imprima esta mensagem no console com template literals dizendo "Você tem x anos;"
+*/
+
+
+function idade(){
+    idade = 21
+    console.log(`Você tem ${idade} anos de idade`)
+}
+
+idade();
