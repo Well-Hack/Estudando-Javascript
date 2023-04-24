d5ed330cccdc417539d1dc4c196f5c721fd2980a
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-04-24 18:44:41 -0300

    exerc24

---
+++ JavaScript/7 Exercicio sobre funções/exerc24.js
@@ -0,0 +1,14 @@
+/*Exercicio 24
+
+*Escreva uma função que receba dois números, o primeiro
+é a base e o segundo a potência;
+
+*Depois faça essa operação e retorne o resultado;
+
+Por exemplo; 3,2 = 9
+
+*/
+
+function potencia(){
+    
+}
