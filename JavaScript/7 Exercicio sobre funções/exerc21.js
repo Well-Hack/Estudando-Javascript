30f95b215c1a4e0711d2d3cda09a8c39f7a72f0d
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-04-24 16:56:28 -0300

    exercicio 21

---
+++ JavaScript/7 Exercicio sobre funções/exerc21.js
@@ -0,0 +1,19 @@
+/* Exercicio 21
+
+*Escreva uma função que detecta o tipo de dado passado;
+
+*Verifique se é um: number, boolean ou string;
+
+*E retorne uma mensagem para cada tipo;
+
+*Execute uma função para cada caso;
+
+ */
+
+function detec(n1){
+    console.log(typeof(n1))
+}
+
+detec(5)
+detec("Olá mundo")
+detec(false)
