23a597f68f6b27cb3306c6a03501cdacf76a9634
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-04-25 17:21:55 -0300

    acessando
    acessando elementos de um array

--- JavaScript/8 Objetos e Arrays/81.js
+++ JavaScript/8 Objetos e Arrays/81.js
@@ -2,5 +2,14 @@ let numeros = [1, 3, 5, 7,]
 
 let info = [21, "teste", true]
 
-console.log(numeros)
-console.log(info)
+
+
+
+//-------------------------------------
+
+let pesquisar = ["Carlos", "Marica", "Luana", "Wellerson"]
+
+console.log(pesquisar)
+console.log(pesquisar[pesquisar.length -1]) // acessar o ultimo dado caso não saiba a casa
+console.log(pesquisar[2])
+console.log(pesquisar[1])
