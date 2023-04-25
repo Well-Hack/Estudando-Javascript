cf781506ad13fc90ff5e58b66fc419a97b81f92d
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-04-25 17:04:10 -0300

    exercicio 25

--- JavaScript/7 Exercicio sobre funções/exerc25.js
+++ JavaScript/7 Exercicio sobre funções/exerc25.js
@@ -3,6 +3,18 @@
 *Escreva uma função que recebe um número, e o decremente
 de 1 em 1 com um loop;
 
-*Além disso imprima somente os números parece no console;
+*Além disso imprima somente os números pares no console;
 
 */
+
+
+function decremente(num){
+    while(num > 0){
+        if(num % 2 == 0){
+        console.log(`${num}`)
+        }
+        num --
+    }
+}
+
+console.log(decremente(15));
