4eec896ea6034f59279b6bb12f7a0c612d8cedce
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-04-26 16:09:10 -0300

    Update 86.js

---
+++ JavaScript/8 Objetos e Arrays/86.js
@@ -0,0 +1,17 @@
+let pessoas = {
+    nome: "Wellerson",
+    idade: 29,
+    profissão: "Programados"
+}
+
+console.log(pessoas)
+console.log(pessoas.idade)
+
+delete pessoas.profissão
+
+console.log(pessoas)
+
+pessoas.sexo = "Masculino"
+
+console.log(pessoas)
+console.log(pessoas.sexo)
