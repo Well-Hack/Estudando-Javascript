8895027ff47c5b923e800a9b9434993dbb3ccc5b
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-04-27 15:48:35 -0300

    8.11 e 8.12

--- JavaScript/8 Objetos e Arrays/811.js
+++ JavaScript/8 Objetos e Arrays/811.js
@@ -1,4 +1,17 @@
 // -------- Metodos de array: Pusj e pop
 
 
-let pessoas = ["Matheus"]
+let pessoas = ["Matheus", "João", "Carlos", "Wellerson"]
+
+console.log(pessoas)
+
+// Remove o elemento no fim do array
+pessoas.pop()
+
+console.log(pessoas)
+
+// Inserir um elemento no fim do array
+pessoas.push("Josefina") 
+
+
+console.log(pessoas)
---
+++ JavaScript/8 Objetos e Arrays/812.js
@@ -0,0 +1,15 @@
+let pessoas = ["Maria", "Emily", "Mariana", "Francisca"]
+
+console.log(pessoas)
+
+
+//Retira elementos no incio da array
+pessoas.shift()
+
+
+console.log(pessoas)
+
+//Adiciona elementos no incio da array
+pessoas.unshift("Wellerson")
+
+console.log(pessoas)
