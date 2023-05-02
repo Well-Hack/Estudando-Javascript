e5a6e14c7180ed70907dbd10a3deaea115ac0a1e
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-05-02 17:24:55 -0300

    Update 818.js

--- JavaScript/8 Objetos e Arrays/818.js
+++ JavaScript/8 Objetos e Arrays/818.js
@@ -1,7 +1,7 @@
 let nomes = "         Matheus";
 
 
-let nome_corrigido = nomes.trim();
+let nome_corrigido = nomes.trim(); //ele remove qualquer elemento que não seja uma string
 
 console.log(nomes)
 console.log(nome_corrigido)
