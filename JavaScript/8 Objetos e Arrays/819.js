07d7c31e373c05e43f39c6742127e560b381c7b1
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-05-03 16:08:56 -0300

    metodo split

---
+++ JavaScript/8 Objetos e Arrays/819.js
@@ -0,0 +1,7 @@
+let sku = "34";
+
+console.log(sku.padStart(6, "0"));
+
+let sku1 = "2578"
+
+console.log(sku1.padStart(6, "0"));
---
+++ JavaScript/8 Objetos e Arrays/820.js
@@ -0,0 +1,4 @@
+let frase = "O rato roeu o rei de Roma"
+
+console.log(frase)
+console.log(frase.split(" "))
