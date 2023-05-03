8e12e6f552e15fb4e082a144b09d12055fbcdd21
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-05-03 16:11:59 -0300

    metodo join

--- JavaScript/8 Objetos e Arrays/820.js
+++ JavaScript/8 Objetos e Arrays/820.js
@@ -1,4 +1,5 @@
 let frase = "O rato roeu o rei de Roma"
 
 console.log(frase)
-console.log(frase.split(" "))
+console.log(frase.split(" "))//Nesse metodo é separado por palavras
+console.log(frase.split("")) //Nesse metodo é separado por letra
