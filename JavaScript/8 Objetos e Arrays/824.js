665a3b68b61606ab94dac7a8e21a6a34f3213758
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-05-04 18:15:00 -0300

    destructuring objetos

---
+++ JavaScript/8 Objetos e Arrays/824.js
@@ -0,0 +1,13 @@
+let obj = {
+    rodas: 4,
+    portas: 4,
+    teto_solar:true,
+    motor: "2.0"
+}
+
+const { rodas: v_rodas, portas: v_portas, teto_solar: v_teto_solar, motor: v_motor} = obj;
+
+console.log(v_rodas)
+console.log(v_portas)
+console.log(v_teto_solar)
+
