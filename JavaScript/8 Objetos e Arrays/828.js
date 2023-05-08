a2c56527cfd54f4aedc3f5ca3f642ae9e3a8e088
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-05-08 17:12:02 -0300

    JSON para string

---
+++ JavaScript/8 Objetos e Arrays/828.js
@@ -0,0 +1,10 @@
+let pessoa = {
+    "Nome": "Matheus",
+    "Idade": 28,
+    "Profissão":"Programador",
+    "Hobbies":["Video game", "leitura", "Correr"]
+}
+
+let pessoatxt = JSON.stringify(pessoa);
+
+console.log(pessoatxt)
