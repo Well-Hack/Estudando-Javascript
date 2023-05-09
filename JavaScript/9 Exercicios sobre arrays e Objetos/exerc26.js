99bdd5b1c7bdd270c37f826de099c9ab0bc6963c
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-05-09 17:42:53 -0300

    exerc26

---
+++ JavaScript/9 Exercicios sobre arrays e Objetos/exerc26.js
@@ -0,0 +1,14 @@
+/* Exercicio 026
+*Crie um array com 5 itens;
+*Acesse o item 1,3,4;
+*Obs: arrays começam no índice 0;
+ */
+
+
+let pessoas = ["Marcelo", "Carlos", "Wellerson", "Luana", "Jennifer"]
+
+console.log(pessoas)
+
+console.log(pessoas[1])
+console.log(pessoas[3])
+console.log(pessoas[4])
