ce4ada2f5e607702a2afb4ba986d47c4542bf884
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-05-09 17:51:52 -0300

    exerc28

---
+++ JavaScript/9 Exercicios sobre arrays e Objetos/exerc28.js
@@ -0,0 +1,24 @@
+/* Exercicio 28
+
+*Crie um objeto onibus;
+
+*Com as propriedades rodas = 8;
+
+*Lmite de passageiros = 40;
+
+*Portas = 2;
+
+Imprima todas as propriedades no console;
+
+*/
+
+
+let onibus = {
+    rodas: 8,
+    limit: 40,
+    portas: 2,
+}
+
+console.log(onibus.rodas)
+console.log(onibus.limit)
+console.log(onibus.portas)
