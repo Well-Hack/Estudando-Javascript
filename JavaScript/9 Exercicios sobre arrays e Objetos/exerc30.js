8c93c913b359e50ded7db246ca32209f606a29c2
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-05-09 18:19:14 -0300

    exerc30

---
+++ JavaScript/9 Exercicios sobre arrays e Objetos/exerc30.js
@@ -0,0 +1,20 @@
+/* Exercicio 30
+
+*Crie um array com 5 nonmes, incluindo o seu;
+
+*Verifique se o seu nome existe no array;
+
+*Se existir imprima alguma mensagem no console;
+
+*/
+
+let nomes = ["Juliana", "Wellerson", "Carlos", "Julia", "Marcia", "Luana"]
+
+let nome2 = nomes.indexOf("Wellerson")
+
+if(nome2 = "Wellerson"){
+    console.log("Nome encontrado")
+}else{
+    console.log("Nome não encontrado")
+}
+
