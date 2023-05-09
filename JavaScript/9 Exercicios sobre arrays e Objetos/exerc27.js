f364c0b33a477bb1f495812125242573394e8b57
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-05-09 17:47:52 -0300

    exerc27

---
+++ JavaScript/9 Exercicios sobre arrays e Objetos/exerc27.js
@@ -0,0 +1,16 @@
+/* Exercicio 27
+
+*Crie um array com 2 elementos e outro com 4;
+
+*Imprima o número de elementoss de cada um na tela;
+
+*/
+
+let elem1 = [1,2]
+
+let elem2 = [1,2,3,4]
+
+console.log(elem1.length)
+
+console.log(elem2.length)
+
