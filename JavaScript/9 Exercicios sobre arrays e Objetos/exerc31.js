cdd4284dd7b2387fbbf8ef018a930e9aadf10c76
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-05-10 18:34:47 -0300

    Create exerc31.js

---
+++ JavaScript/9 Exercicios sobre arrays e Objetos/exerc31.js
@@ -0,0 +1,18 @@
+/* Exercicio 31
+
+* Crie dois arrays, um com mais de 5 elementos e outro com menos,
+
+* Faça uma função que verifica o número de elementos;
+
+* Se possuir menos que 5, imprima "poucos elementos" no console;
+
+* Se tiver mais, imprima "Muitos elementos"
+
+*/ 
+
+
+let arr1 = [1,2,3,4,5,6]
+
+let arr2 = [1,2,3,4]
+
+fuction verificar_elementos(arr)
