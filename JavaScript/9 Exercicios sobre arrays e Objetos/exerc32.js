d30e8e68bd661d255e465dff1c7ee61911321038
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-05-10 21:21:12 -0300

    Exercicio 32

---
+++ JavaScript/9 Exercicios sobre arrays e Objetos/exerc32.js
@@ -0,0 +1,14 @@
+/* EXERCICIO 32
+
+*Crie um array com 5 elementos;
+
+Faça uma interação entre todos eles e imprima no console o valor;
+
+*/
+
+
+let nomes = ["Carvalho", "Mario", "José", "Silvio", "Claudio"]
+
+for(let i = 0; i <= nomes.length - 1; i++){
+    console.log(`Olá ${nomes[i]}`)
+}
