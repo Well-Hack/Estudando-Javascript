0ddfa7b93c08351b093abfe049eed6f14273edb9
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-05-12 19:29:49 -0300

    Update 102.js

---
+++ JavaScript/10 Conceitos de Orientação a Objeto/102.js
@@ -0,0 +1,14 @@
+const cachorro = {
+    latir : function(){
+        console.log("Au Au")
+    },
+    chorar: function(){
+        console.log("auuuuuu")
+    }
+}
+
+console.log(cachorro.latir())
+
+// pode também imprimir sem o console.log
+
+cachorro.chorar()
