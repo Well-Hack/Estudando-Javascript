c746a4abbb1e1041bebf478a28472d2d6364e01d
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-05-16 16:47:43 -0300

    aprofundando em métodos

---
+++ JavaScript/10 Conceitos de Orientação a Objeto/103.js
@@ -0,0 +1,11 @@
+const gato = {
+    miar: function(){
+        console.log("Miaau")
+    },
+    raiva: function(){
+        console.log("Miaaaaaaaaaaaaaaaaau")
+    }
+}
+
+gato.miar()
+gato.raiva()
