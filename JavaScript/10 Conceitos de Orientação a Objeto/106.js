30012fe8ad08030ada169c16ba950e8cfcaebd00
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-05-16 18:00:06 -0300

    Update 106.js

--- JavaScript/10 Conceitos de Orientação a Objeto/106.js
+++ JavaScript/10 Conceitos de Orientação a Objeto/106.js
@@ -13,3 +13,5 @@ const pessoa_nova = Object.create(pessoa);
 console.log(pessoa_nova.maos);
 
 console.log(pessoa_nova.hasOwnProperty("maos"))
+
+console.log(Object.getPrototypeOf(pessoa_nova) === pessoa)
