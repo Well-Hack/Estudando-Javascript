4b8662e3b1e319af919c0b2ae6d5c3fd6067e453
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-05-16 17:58:34 -0300

    mais sobre prototype
    aula complicada de se aprender

---
+++ JavaScript/10 Conceitos de Orientação a Objeto/106.js
@@ -0,0 +1,15 @@
+const pessoa = {
+    maos: 2,
+}
+
+//console.log(Object.getPrototypeOf(pessoa))
+//console.log(Object.getPrototypeOf(pessoa) ===  Object.prototype)
+
+
+//console.log(pessoa.hasOwnProperty("maos"));
+
+const pessoa_nova = Object.create(pessoa);
+
+console.log(pessoa_nova.maos);
+
+console.log(pessoa_nova.hasOwnProperty("maos"))
