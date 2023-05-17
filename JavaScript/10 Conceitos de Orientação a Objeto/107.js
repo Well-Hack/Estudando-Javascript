44fe7dd38c5e7553d06bf3e8431c181014114439
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-05-17 15:18:20 -0300

    Update 107.js

--- JavaScript/10 Conceitos de Orientação a Objeto/107.js
+++ JavaScript/10 Conceitos de Orientação a Objeto/107.js
@@ -14,3 +14,9 @@ labrador.raca = "labrador"
 
 console.log(labrador.raca);
 console.log(cachorro.raca)
+
+
+let pastor_alemao = Object.create(cachorro);
+pastor_alemao.raca = "Pastor alemão";
+
+console.log(pastor_alemao.raca)
