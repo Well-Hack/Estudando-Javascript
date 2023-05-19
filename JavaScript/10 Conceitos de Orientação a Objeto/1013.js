8c46edb900042006bf57b0f6b4e297c1c6edf521
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-05-18 22:13:11 -0300

    Override

---
+++ JavaScript/10 Conceitos de Orientação a Objeto/1013.js
@@ -0,0 +1,18 @@
+class Cachorro {
+    constructor(raca,patas,cor){
+        this.raca = raca;;
+        this.cor = cor;
+    };
+    latir(){
+        console.log("Au au")
+    }
+}
+
+Cachorro.prototype.raca = "SRD"
+Cachorro.prototype.patas = 4;
+
+let labrador = new Cachorro("labrador", "Amarelo")
+console.log(labrador.patas)
+labrador.latir()
+
+console.log(Cachorro.prototype.raca);
