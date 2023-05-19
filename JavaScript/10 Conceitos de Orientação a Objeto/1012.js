de130a3eb0dfe866c359fb19da62ad228e48bca3
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-05-18 22:10:09 -0300

    10.12

---
+++ JavaScript/10 Conceitos de Orientação a Objeto/1012.js
@@ -0,0 +1,15 @@
+class Cachorro {
+    constructor(raca,patas,cor){
+        this.raca = raca;;
+        this.cor = cor;
+    };
+    latir(){
+        console.log("Au au")
+    }
+}
+
+Cachorro.prototype.patas = 4;
+
+let labrador = new Cachorro("labrador", "Amarelo")
+console.log(labrador.patas)
+labrador.latir()
