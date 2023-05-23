e92db4da63ec2b77c0931cda17464a495c83cebb
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-05-23 19:23:21 -0300

    exerc39

---
+++ JavaScript/11 Exercicios sobre orientação a objetos/exerc39.js
@@ -0,0 +1,20 @@
+/* Exercicio 04
+
+*Crie uma classe que simule um carro;
+
+*Propriedades: marca, cor, gasolina restante;
+
+*Crie um método de dirigir o carro, que vá diminuindo a gasolina
+gradativamente
+
+*E um de abastecer para aumentar a gasolina quando necessário;
+
+*/
+
+class Carro{
+    constructor(marca, cor, gasolres){
+        this.marca = marca;
+        this.cor = cor;
+        this.gasolres = gasolres;
+    }
+}
