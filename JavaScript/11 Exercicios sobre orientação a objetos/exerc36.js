755b69d541dba4976f5bdfd0904ce29af354132c
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-05-22 21:07:05 -0300

    Exercicios sobre orientação a objeto

---
+++ JavaScript/11 Exercicios sobre orientação a objetos/exerc36.js
@@ -0,0 +1,11 @@
+/* Exercicio 36
+
+*Crie uma classe que simula uma conta no banco (utilize a forma que
+    preferir);
+*Deve conter a propriedade saldo:
+
+*E os métodos depositos e saque
+
+*Teste os métodos
+
+*/
