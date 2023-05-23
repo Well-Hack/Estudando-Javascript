27607e8267e2cedadce22d499553e67e2e878334
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-05-23 18:41:29 -0300

    exerc38

---
+++ JavaScript/11 Exercicios sobre orientação a objetos/exerc38.js
@@ -0,0 +1,20 @@
+/* Exercicio 37
+
+*Crie um objeto que simula um endereço de um cliente;
+
+*Propriedades: Rua, Bairro, Cidade e Estado;
+
+*No constructor o endereço já deve ser definicido por completo;
+
+*Crie métodos para atualizar todas as propriedades;
+
+*/
+
+class Endereco{
+    constructor(Rua, Bairro, Cidade, Estado){
+        this.Rua = Rua,
+        this.Bairro = Bairro,
+        this.Cidade = Cidade,
+        this.Estado = Estado
+    }
+}
