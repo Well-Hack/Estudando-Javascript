f7195ac60987a67738538bd3caee454c29b56e67
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-05-26 17:59:29 -0300

    Update 123.js

---
+++ JavaScript/12 Debugs e erros no javascript/123.js
@@ -0,0 +1,21 @@
+let a = 1;
+let b = 2;
+let c = 3;
+
+if(c > a){
+    a = b;
+}
+
+for(let i = 5; i > 0; i --){
+    b++;
+    a = a + c + 1;
+    c += 2;
+}
+
+if(b == a){
+    a++;
+} else {
+    a = a + b + c;
+}
+
+a = a * b;
